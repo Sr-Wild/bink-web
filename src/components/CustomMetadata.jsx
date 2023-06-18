ae9cd9c8c11a06f4bd90d8f50daca04220f2b2b5
alex1leal <alexander1leal@gmail.com>
2023-06-18 17:58:42 -0400

    Incorporar icono (FALLÓ)

--- src/components/CustomMetadata.jsx
+++ src/components/CustomMetadata.jsx
@@ -1,10 +1,11 @@
 import Head from 'next/head'
+import Icon from '@/assets/favicon.ico'
 
 /**
- * @param title {string} - Título de la página, por defecto lleva el título "Principal".
+ * @param title {string} - Título de la página, por defecto lleva el título 'Principal'.
  * @param description {string} - Descripción de la página.
  * @param keywords {Array} - Palabras claves que describen el proyecto. Todo en un array de strings.
- * @param author {string} - Autor de la página, por defecto el nombre del equipo "Bink".
+ * @param author {string} - Autor de la página, por defecto el nombre del equipo 'Bink'.
  */
 const CustomMetadata = ({
   title = 'Principal',
@@ -22,6 +23,7 @@ const CustomMetadata = ({
       <meta name='keywords' content={keys} />
       <meta name='author' content={author} />
       <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      <link rel='icon' href='/favicon.ico'/>
     </Head>
   )
 }
